5b1e3b523090f9964ff4b05404b32d52a296c506
it's_Mariga <123575542+BenjaminMariga@users.noreply.github.com>
2023-01-28 19:03:47 +0300

    Add files via upload

---
+++ 01intro.dart
@@ -0,0 +1,10 @@
+void main() {
+  //print  a variable
+  //print hello dart
+  int num = 2;
+  String intro = "Hello Welcome to dart";
+  print(num);
+  print(intro);
+  //print as a String
+  print("Greetings:$intro number is:$num");
+}
